5eaf75646378bf09b199476cb33930fa79fa01a1
Manuel E. Hernandez <kicke10h@gmail.com>
2022-03-19 17:58:13 -0600

    Change the path upload for save a file

--- backend/src/routes/files.py
+++ backend/src/routes/files.py
@@ -1,11 +1,28 @@
-from fastapi import APIRouter
+from os import getcwd, mkdir, path, remove
+from fastapi import APIRouter, UploadFile, File
 from modules.excel.convert import xlxs2Tuples
 from modules.patterns.minimum_distance import get_class
 
 files = APIRouter()
 
 @files.post("/upload")
-def upload(path_file: str) -> int:
-    """This path get the class of a pattern with the data in excel"""
-    return get_class(xlxs2Tuples(path_file, 1)[0], xlxs2Tuples(path_file, 0))
-    
+async def upload(file: UploadFile = File(...)) -> int:
+    """This path saves a temporary file and get the pattern's class with data in the excel file then remove the file"""
+    uri = path.abspath(path.join(getcwd(),"../files/"))
+    # If the folder doesn't exist created'
+    try:
+        mkdir(uri)
+    except OSError:
+        pass
+    #  Set the the complete path
+    uri += "/"+file.filename
+    # Save the excel file
+    with open(uri, "wb") as my_file:
+        content = await file.read()
+        my_file.write(content)
+        my_file.close()
+    # Get the number of class 
+    c_value = get_class(xlxs2Tuples(uri,1)[0], xlxs2Tuples(uri,0))
+    # Delete the excel file
+    remove(uri)
+    return c_value
