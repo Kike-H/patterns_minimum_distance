02c4dc19fd153e27c995013f98948c39489bffaf
Manuel E. Hernandez <kicke10h@gmail.com>
2022-03-19 16:03:06 -0600

    Make the path for get value minum distance

--- backend/src/routes/files.py
+++ backend/src/routes/files.py
@@ -1,9 +1,11 @@
-from fastapi import APIRouter, UploadFile, File
+from fastapi import APIRouter
+from modules.excel.convert import xlxs2Tuples
+from modules.patterns.minimum_distance import get_class
 
 files = APIRouter()
 
 @files.post("/upload")
-def upload(file: UploadFile = File(...)) -> str:
+def upload(path_file: str) -> int:
     """This path get the class of a pattern with the data in excel"""
-    return file.filename
-
+    return get_class(xlxs2Tuples(path_file, 1)[0], xlxs2Tuples(path_file, 0))
+    
