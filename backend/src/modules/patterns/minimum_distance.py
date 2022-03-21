a1d4cfd5aa76edb020424be8825bd9c8d82f7d7e
Manuel E. Hernandez <kicke10h@gmail.com>
2022-03-21 08:54:12 -0600

    Change the varibles name

--- backend/src/modules/patterns/minimum_distance.py
+++ backend/src/modules/patterns/minimum_distance.py
@@ -4,14 +4,14 @@ def get_class(pattern: tuple[float], data: tuple[tuple[float]]) -> int:
 
     # Get all the vectors in data
     for vector in data:
-        sum_1 = 0
-        sum_2 = 0
+        pp_1 = 0
+        pp_2 = 0
         # Make product point for both parts
         for i in range(0,len(vector)-1):
-            sum_1 += vector[i]*pattern[i]
-            sum_2 += vector[i]*vector[i]
+            pp_1 += vector[i]*pattern[i]
+            pp_2 += vector[i]*vector[i]
          # Calculate the max value between the two vectors and to values 
-        values.append(sum_1-(0.5*sum_2)) 
+        values.append(pp_1-(0.5*pp_2)) 
     # Get the index of maximum value and get the class in the same index in data 
     return int(data[values.index(max(values))][-1])
 
